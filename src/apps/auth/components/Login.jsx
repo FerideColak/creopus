c8df0e27410a2bbbf9743c0a6b691340cf4745b0
FerideColak <feride_colak577@hotmail.com>
2023-06-20 11:09:42 +0300

    Add login page

--- src/apps/auth/components/Login.jsx
+++ src/apps/auth/components/Login.jsx
@@ -1,15 +1,64 @@
-import {useContext} from "react";
-//CONTEXT
-import FirebaseContext from "../../../context/FirebaseContext";
+import {useContext, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+// CONTEXT
+import FirebaseContext from '../../../context/FirebaseContext'
+// STYLE
+import styles from '../styles/login.module.css'
 
 const Login = () => {
+    const navigate = useNavigate()
+    const {login} = useContext(FirebaseContext)
+    const {email, setEmail} = useState('')
+    const {password, setPassword} = useState('')
     return (
-       <>
-        <form action="">
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-        </form>
-       </>
+       <div className={styles.login_container}>
+            {/* BANNER */}
+            <div className={styles.inner_container}>
+                <div className={styles.banner_container}>
+                {/* FORM */}
+                <form>
+                    <h2>Sign In to Your Account</h2>
+                    {/* SOCIAL BUTTONS */}
+                    <div className={styles.social_login_container}>
+                        <p>Sign in using social networks</p>
+                        <div className={styles.social_login_buttons}>
+                            
+                            <button>
+                                <i className='fa-brands fa-google'></i>
+                            </button>
+                            <button>
+                                <i className = 'fab fa-facebook-f'></i>
+                            </button>
+                            <button>
+                                <i className='fa-brands fa-twitter'></i>
+                            </button>
+                        </div>
+                    </div>
+                    {/* SEPERATOR */}
+                    <div className={styles.seperator}>
+                        <div className={styles.line}></div>
+                        <p>or</p>
+                        <div className={styles.line}></div>
+                    </div>
+                    <div className={styles.form_control}>
+                        <input type='email'name='user_email' placeholder={'Email'}
+                        onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className={styles.form_control}>
+                        <input type='password' name='user_password' placeholder={'Password'}
+                        onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    {/* BUTTON */}
+                    <button className={[styles.login_btn, styles.btn].join(' ')}
+                            onClick = {(e) => {
+                                e.preventDefault()
+                                login(email, password)
+                            }}
+                            >Login</button>
+                </form>
+                </div>
+            </div>
+       </div>
     )
 }
 
